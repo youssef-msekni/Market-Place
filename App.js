93e315e78e892ee174cda502c1c24e0bb630630c
unknown <y.msekni49@gmail.com>
2023-06-15 22:47:08 +0100

    update

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { StyleSheet } from 'react-native'
+import SignUp from './src/SignUp'
+import SignUppPageTwo from './src/SignUpPageTwo'
+import WelcomePage from './src/welcomePage'
+import Settings from './src/settings'
+
+
+
+const App =()=> {
+  return(
+    <Settings/>
+  )
+}
+const styles = StyleSheet.create( {
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    
+    
+  }});
+
+export default App
